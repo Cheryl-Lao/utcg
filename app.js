cae2f4ab5c005abd4c05f0ed800aaf35119b7ac5
Jena Choi <hyewonchoi11@gmail.com>
2016-09-12 18:59:07 -0400

    add things to

--- app.js
+++ app.js
@@ -47,37 +47,6 @@ app.get('/', function(req, res) {
   res.render('home', {
     title:'UTCG: Welcome to U of T Computer Graphics Club',
     content: `
-    <h1>Welcome to the University of Toronto Computer Graphics Club.</h1>
-
-    <p>Computer graphics design really isn't difficult at all when you get like-minded people to help you out. Whether you’re just starting out or looking for new techniques to inspire you, UTCG is here to help. Come join our workshops and learn all about the Adobe suite and more. Sure, you could watch YouTube videos and read all about it but it really doesn’t compare to learning together. It doesn’t matter your area of study, or whether you’re a student, faculty, staff or alumni, just come along, ask questions, and share your experiences!
-
-    <h3>Focuses:</h3>
-
-    <p>Adobe Creative Suite (Photoshop, Photoshop Lightroom, Illustrator, InDesign)
-    <p>Sketchup
-    <p>Wordpress and web design
-    <p>Layout principles
-    <p>Digital painting
-    <p>...and whatever else you want to learn!
-
-    <h3>Workshops: When and where?</h3>
-
-    <p>Our workshops are held from 3-5pm at WE51A, Wetmore Hall, New College at the University of Toronto. The complete address is as follows:
-    <p>Wetmore Hall, New College
-    <p>300 Huron Street
-    <p>Toronto M5S 2Z3
-
-    <p>Wetmore Hall, New College
-
-    <h3>About the UTCG Group.</h3>
-
-    <p>We’re a Ulife and UTSU recognized campus group at the University of Toronto. Our main aim is to prove to people how easy it is to learn graphic design, and how applicable it is in all academic disciplines. If you have any suggestions, questions, comments, feedback...email us at <a href='mailto:communication@utcg.org'>communication@utcg.org</a>
-    .
-
-    <p>Call us or beep us whenever you want to reach us.
-
-    <p>There are a variety of means through which you can reach us. The quickest and easiest is email, followed by Facebook. To see what we're upto, check out our Twitter.
-
     `
   });
 })
---
+++ public/js/hover.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+  $("#processing").hover(function(){
+    $(".appear").text("Processing");
+  });
+  $("#processing").mouseleave(function(){
+    $(".appear").text("");
+  });
+
+  $("#webdesign").hover(function(){
+    $(".appear").text("Web");
+  });
+  $("#webdesign").mouseleave(function(){
+    $(".appear").text("");
+  });
+
+  $("#programming").hover(function(){
+    $(".appear").text("Programming");
+  });
+  $("#programming").mouseleave(function(){
+    $(".appear").text("");
+  });
+
+
+
+  $("#ideas").hover(function(){
+    $(".appear").text("ideas");
+  });
+  $("#ideas").mouseleave(function(){
+    $(".appear").text("");
+  });
+
+  $("#projects").hover(function(){
+    $(".appear").text("projects");
+  });
+  $("#projects").mouseleave(function(){
+    $(".appear").text("");
+  });
+
+  $("#connection").hover(function(){
+    $(".appear").text("connection");
+  });
+
+  $("#connection").mouseleave(function(){
+    $(".appear").text("");
+  });
+
+  // appear 2
+
+  $("#processing").hover(function(){
+    $(".appear2").text("learn the processing by attending our workshop!");
+  });
+
+  $("#processing").mouseleave(function(){
+    $(".appear2").text("");
+  });
+
+  $("#webdesign").hover(function(){
+    $(".appear2").text("Small step to building beautiful and interactive website by yourself");
+  });
+
+  $("#webdesign").mouseleave(function(){
+    $(".appear2").text("");
+  });
+
+  $("#programming").hover(function(){
+    $(".appear2").text("Graphics integrated with programming");
+  });
+
+  $("#programming").mouseleave(function(){
+    $(".appear2").text("");
+  });
+
+  $("#ideas").hover(function(){
+    $(".appear2").text("Get to share and develop ideas with your teammates");
+  });
+
+  $("#ideas").mouseleave(function(){
+    $(".appear2").text("");
+  });
+
+  $("#projects").hover(function(){
+    $(".appear2").text("Build on technical projects");
+  });
+
+  $("#projects").mouseleave(function(){
+    $(".appear2").text("");
+  });
+
+  $("#connection").hover(function(){
+    $(".appear2").text("Get to build connection by attending field trip to amazing graphics companies");
+  });
+
+  $("#connection").mouseleave(function(){
+    $(".appear2").text("");
+  });
+});
